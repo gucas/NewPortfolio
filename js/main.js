77693819692c10c1c496dffc308a71764924add8
gucas <gucasphoto@mac.com>
2016-01-16 21:36:46 +0100

    Add Nav Contact

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$('.burger-btn').click(function(){
 		$(this).toggleClass('active');
+		$('.wrapper-header').toggleClass('active');
 		$('.nav-box').toggleClass('active');
 	});
+
+	// $('.nav-link').click(function(){
+	// 	$(this).toggleClass('active');
+	// });
 });
--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".burger-btn").click(function(){$(this).toggleClass("active"),$(".nav-box").toggleClass("active")})});
+$(document).ready(function(){$(".burger-btn").click(function(){$(this).toggleClass("active"),$(".wrapper-header").toggleClass("active"),$(".nav-box").toggleClass("active")})});
